dbb7c4ea3dc6a2072f800b1369ec2e2788db4b00
Mehboob-Alam1 <ma8122576@gmail.com>
2024-01-19 05:10:05 -0800

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const App());
+}
+
+
+class App extends StatelessWidget {
+  const App({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return  MaterialApp(
+      theme: ThemeData(
+//TODO   was in themes section
+      ),
+    );
+  }
+}
+
