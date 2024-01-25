b76455056e0306262fa8b6052682450953f2051a
Mehboob-Alam1 <ma8122576@gmail.com>
2024-01-25 18:37:08 +0500

    added search container and section heading reusable widgets

---
+++ lib/common/widgets/custom_shapes/containers/search_container.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+import 'package:iconsax/iconsax.dart';
+
+import '../../../../utils/constants/colors.dart';
+import '../../../../utils/constants/sizes.dart';
+import '../../../../utils/devices/device_utility.dart';
+import '../../../../utils/helpers/helper_functions.dart';
+
+class TSearchContainer extends StatelessWidget {
+  const TSearchContainer({
+    super.key, required this.text, this.icon=Iconsax.search_normal,  this.showBackground=true,  this.showBorder=true,
+  });
+  final String text;
+  final IconData? icon;
+  final bool showBackground,showBorder;
+
+  @override
+  Widget build(BuildContext context) {
+
+    final dark =THelperFunctions.isDarkMode(context);
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: TSizes.defaultSpace),
+      child: Container(
+        width: TDeviceUtils.getScreenWidth(context),
+        padding: const EdgeInsets.all(TSizes.md),
+        decoration: BoxDecoration(
+            color: showBackground?dark? TColor.dark :TColor.light:Colors.transparent,
+            borderRadius: BorderRadius.circular(TSizes.cardRadiusLg),
+            border:showBorder? Border.all(color: TColor.grey) :null
+        ),
+        child:  Row(
+          children: [
+            Icon(icon,color: TColor.darkerGrey),
+            const SizedBox(width: TSizes.spaceBtwItems),
+            Text(text,style: Theme.of(context).textTheme.bodySmall,)
+          ],
+        ),
+      ),
+
+
+    );
+  }
+}
--- lib/features/shop/screens/home/home.dart
+++ lib/features/shop/screens/home/home.dart
@@ -2,20 +2,46 @@ import 'package:ecommerce/features/shop/screens/home/widgets/home_appbar.dart';
 import 'package:flutter/material.dart';
 
 import '../../../../common/widgets/custom_shapes/containers/primary_header_container.dart';
+import '../../../../common/widgets/custom_shapes/containers/search_container.dart';
+import '../../../../common/widgets/text/section_heading.dart';
+import '../../../../utils/constants/sizes.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return   const Scaffold(
+    return const Scaffold(
       body: SingleChildScrollView(
         child: Column(
           children: [
             TPrimaryHeaderContainer(
-              child:Column(
+              child: Column(
                 children: [
-                  THomeAppBar()
+                  ///Appbar
+                  THomeAppBar(),
+
+                  SizedBox(height: TSizes.spaceBtwSections),
+
+                  ///Search bar
+
+                  TSearchContainer(text: "Search in store"),
+                  SizedBox(height: TSizes.spaceBtwSections),
+
+                  ///Categories
+
+                  Padding(
+                    padding: EdgeInsets.only(left: TSizes.defaultSpace),
+                    child: Column(
+                      children: [
+                        TSectionHeading(
+                          title: 'Popular Categories',
+
+                        ),
+                        SizedBox(height: TSizes.spaceBtwItems),
+                      ],
+                    ),
+                  )
                 ],
               ),
             ),
@@ -25,5 +51,3 @@ class HomeScreen extends StatelessWidget {
     );
   }
 }
-
-
