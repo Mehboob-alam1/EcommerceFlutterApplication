0212a1edd2d03254d12b1d4f43795654c293aabf
Mehboob-Alam1 <ma8122576@gmail.com>
2024-01-25 15:18:00 +0500

    clip path custom shape added and extracted the widgets the primary header container is set

--- lib/features/shop/screens/home/home.dart
+++ lib/features/shop/screens/home/home.dart
@@ -1,12 +1,22 @@
 import 'package:flutter/material.dart';
 
-
+import '../../../../common/widgets/custom_shapes/containers/primary_header_container.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return  Scaffold(
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            TPrimaryHeaderContainer(
+              child:Container(),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
